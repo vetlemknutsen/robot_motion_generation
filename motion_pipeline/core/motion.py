a4a557a656c6875320525477e04feeeadf8f7d27
Vetle Knutsen <vetleknutsen1@gmail.com>
2025-11-25 15:22:04 +0100

    added EndEffectorTarget for future IK

--- motion_pipeline/core/motion.py
+++ motion_pipeline/core/motion.py
@@ -9,12 +9,19 @@ class JointAngle:
     degrees: float
     side: Optional[str] = None
 
+@dataclass
+class EndEffectorTarget:
+    side: str
+    position: List[float] 
+    orientation: Optional[List[float]] = None  
+
 
 # A list of JointAngle's for a frame
 @dataclass
 class PoseFrame:
     time: float
     joint_angles: List[JointAngle] = field(default_factory=list)
+    targets: List[EndEffectorTarget] = field(default_factory=list)
 
 
 # List of frames = Motion
